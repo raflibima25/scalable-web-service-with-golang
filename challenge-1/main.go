879c2fbbc1333bfbcc391389a9810832dc9078eb
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-03-10 08:51:12 +0700

    update make clean code

--- challenge-1/main.go
+++ challenge-1/main.go
@@ -2,36 +2,52 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	i := 21
-	var j = true
 
+	// menampilkan nilai i = 21
+	i := 21
 	fmt.Println(i)
+
+	// menampilkan tipe data dari variabel i
 	fmt.Printf("%T \n", i)
+
+	// menampilkan tanda %
 	fmt.Println("%")
 
+	// menampilkan nilai boolean j : true
+	j := true
 	fmt.Println(j)
+
+	// menampilkan nilai boolean j : false
 	j = false
 	fmt.Println(j)
 
-	// fmt.Printf("%c \n", "я")
-	// r, size := utf8.DecodeRuneInString("я")
-	// 	fmt.Printf("%c %v\n", r, size)
-
-	// rune, _ := utf8.DecodeRuneInString("я")
-	// fmt.Println(rune)
-
+	// menampilkan unicode russia : Я (ya)
 	fmt.Printf("%U \n", []rune("Я"))
 
+	// menampilkan nilai base 10 : 21
 	fmt.Printf("%d \n", 21)
+
+	// menampilkan nilai base 8 :25
 	fmt.Printf("%o \n", 25)
+
+	// menampilkan nilai base 16 : f
 	fmt.Printf("%x \n", "f")
+
+	// menampilkan nilai base 16 : F 13
 	fmt.Printf("%X \n", "F")
 
-	// fmt.Printf("%U", "U+042F")
+	// menampilkan unicode karakter Я : U+042F
+	x, _ := strconv.ParseInt("042F", 16, 64)
+	fmt.Printf("%U \n", x)
+
+	// menampilkan float : 123.456000
 	fmt.Printf("%f \n", 123.456000)
+
+	// menampilkan float scientific : 1.234560E+02
 	fmt.Printf("%e \n", 1.234560e+02)
 
 }
